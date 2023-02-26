92ebd96b45eb1559d2a982c09c26b4d3b3e5e53f
dashechka <dashechka@DESKTOP-0PMQMUJ>
2023-02-26 19:48:40 +0300

    Добавьте файлы проекта.

---
+++ MonitoringProgres/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MonitoringProgres.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
